9cec8387acb0af1f18b2ab47b9608192478e502a
stefanvanderheijden <stefanvanderheijden@gmail.com>
2020-07-05 22:08:43 +0200

    c

--- webserver/my-website.py
+++ webserver/my-website.py
@@ -5,13 +5,9 @@ import Adafruit_DHT as dht
 app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
-led1 = 21  
-led2 = 20 
+
 DHT22_pin = 4
 
-# Set each pin as an output and make it low:
-GPIO.setup(led1, GPIO.OUT)
-GPIO.setup(led2, GPIO.OUT)
 
 @app.route("/")
 
@@ -23,28 +19,29 @@ def main():
 def action(pin, action):
    temperature = ''
    humidity = ''
-   if pin == "pin1" and action == "on":
-      GPIO.output(led1, GPIO.HIGH)
-   
-   if pin == "pin1" and action == "off":
-      GPIO.output(led1, GPIO.LOW)
-   
-   if pin == "pin2" and action == "on":
-      GPIO.output(led2, GPIO.HIGH)
-   
-   if pin == "pin2" and action == "off":
-      GPIO.output(led2, GPIO.LOW)
 
    if pin == "dhtpin" and action == "get":
       humi, temp = dht.read_retry(dht.DHT22, DHT22_pin)  # Reading humidity and temperature
-      humi = '{0:0.1f}' .format(humi)
-      temp = '{0:0.1f}' .format(temp)
+
+      h1, t1 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+      h2, t2 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 5)
+      h3, t3 = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 26)
+
+      h1 = round(h1, 2)
+      t1 = round(t1, 2)
+
+      h2 = round(h2, 2)
+      t2 = round(t2, 2)
+
+      h3 = round(h3, 2)
+      t3 = round(t3 ,2)
+
       temperature = 'Temperature: ' + temp 
       humidity =  'Humidity: ' + humi
 
    templateData = {
-   'temperature' : temperature,
-   'humidity' : humidity
+   't1' : temperature,
+   'h1' : humidity
    }
 
    return render_template('main.html', **templateData)
