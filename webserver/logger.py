5ab0dccfb0f4877993531ac504015fddd7e760df
Stefan <stefanvanderheijden@gmail.com>
2020-07-15 21:21:21 +0200

    ds

--- webserver/logger.py
+++ webserver/logger.py
@@ -18,21 +18,26 @@ GPIO.setmode(GPIO.BCM)
 DHT22_pin = 4
 
 while True:
-    h1, t1 = dht.read_retry(dht.DHT22, 4)
-    h2, t2 = dht.read_retry(dht.DHT22, 5)
-    h3, t3 = dht.read_retry(dht.DHT22, 26)
+    try: 
+        h1, t1 = dht.read_retry(dht.DHT22, 4)
+        h2, t2 = dht.read_retry(dht.DHT22, 5)
+        h3, t3 = dht.read_retry(dht.DHT22, 26)
 
-    h1 = round(h1, 2)
-    t1 = round(t1, 2)
+        h1 = round(h1, 2)
+        t1 = round(t1, 2)
 
-    h2 = round(h2, 2)
-    t2 = round(t2, 2)
+        h2 = round(h2, 2)
+        t2 = round(t2, 2)
 
-    h3 = round(h3, 2)
-    t3 = round(t3 ,2)
+        h3 = round(h3, 2)
+        t3 = round(t3 ,2)
 
-    now = datetime.now()
-    file.open()
-    file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
-    print("I have logged the temperature to a CSV file")
-    time.sleep(5)
+        now = datetime.now()
+        file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
+        print("I have logged the temperature to a CSV file")
+        time.sleep(5)
+    
+    except KeyboardInterrupt:
+        print "Bye"
+        file.close
+        sys.exit()
