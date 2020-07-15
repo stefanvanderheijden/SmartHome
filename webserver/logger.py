2427083b9a49109026124e2ea1c6a010faf684be
Stefan <stefanvanderheijden@gmail.com>
2020-07-15 21:19:39 +0200

    ds

--- webserver/logger.py
+++ webserver/logger.py
@@ -35,6 +35,4 @@ while True:
     file.open()
     file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
     print("I have logged the temperature to a CSV file")
-    file.flush()
     time.sleep(5)
-    file.close()
