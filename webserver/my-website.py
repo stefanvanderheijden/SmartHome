9114d42b57e2d4505619320fbb6a31d1cb05553e
Stefan <stefanvanderheijden@gmail.com>
2020-07-15 20:51:27 +0200

    ds

--- webserver/my-website.py
+++ webserver/my-website.py
@@ -7,10 +7,12 @@ import time
 from time import sleep
 from datetime import datetime
 
-file = open("/home/pi/Documents/Script/Data/data_log.csv", "a")
+file = open("/home/pi/data_log.csv", "a")
+print("opened file")
 
 if os.stat("/home/pi/data_log.csv").st_size == 0:
-        file.write("Tijd,BuitenTemp,BuitenLuchtV,HuiskamerTemp,HuiskamerLuchtV,KantoorTemp,KantoorLuchtV\n")
+   print("added headers to data log file")
+   file.write("Tijd,BuitenTemp,BuitenLuchtV,HuiskamerTemp,HuiskamerLuchtV,KantoorTemp,KantoorLuchtV\n")
 
 app = Flask(__name__)
 
@@ -65,27 +67,28 @@ def action(pin, action):
 
    return render_template('main.html', **templateData)
 
+while True:
+   h1, t1 = dht.read_retry(dht.DHT22, 4)
+   h2, t2 = dht.read_retry(dht.DHT22, 5)
+   h3, t3 = dht.read_retry(dht.DHT22, 26)
+
+   h1 = round(h1, 2)
+   t1 = round(t1, 2)
+
+   h2 = round(h2, 2)
+   t2 = round(t2, 2)
+
+   h3 = round(h3, 2)
+   t3 = round(t3 ,2)
+
+   now = datetime.now()
+   file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
+   print("I have logged the temperature to a CSV file")
+   file.flush()
+   file.close()
+   time.sleep(5)
+
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
-   while True:
-        
-        
-        h1, t1 = dht.read_retry(dht.DHT22, 4)
-        h2, t2 = dht.read_retry(dht.DHT22, 5)
-        h3, t3 = dht.read_retry(dht.DHT22, 26)
+   print("app host started")
 
-        h1 = round(h1, 2)
-        t1 = round(t1, 2)
-
-        h2 = round(h2, 2)
-        t2 = round(t2, 2)
-
-        h3 = round(h3, 2)
-        t3 = round(t3 ,2)
-
-        now = datetime.now()
-        file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
-        print("I have logged the temperature to a CSV file")
-        file.flush()
-        file.close()
-        time.sleep(5)
