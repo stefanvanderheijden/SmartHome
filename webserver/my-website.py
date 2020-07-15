dad07905f6a01c49ce1e2f9dda03f858d89ae229
Stefan <stefanvanderheijden@gmail.com>
2020-07-15 20:17:18 +0200

    added data logging

--- webserver/my-website.py
+++ webserver/my-website.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
 
+import os
+import time 
+from time import sleep
+from datetime import datetime
+
+file = open("/home/pi/data_log.csv", "a")
+
+if os.stat("/home/pi/data_log.csv").st_size == 0:
+        file.write("Tijd,BuitenTemp,BuitenLuchtV,HuiskamerTemp,HuiskamerLuchtV,KantoorTemp,KantoorLuchtV\n")
+
 app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
@@ -56,3 +66,22 @@ def action(pin, action):
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
+
+while True:
+        now = datetime.now()
+        h1, t1 = dht.read_retry(dht.DHT22, 4)
+        h2, t2 = dht.read_retry(dht.DHT22, 5)
+        h3, t3 = dht.read_retry(dht.DHT22, 26)
+
+        h1 = round(h1, 2)
+        t1 = round(t1, 2)
+
+        h2 = round(h2, 2)
+        t2 = round(t2, 2)
+
+        h3 = round(h3, 2)
+        t3 = round(t3 ,2)
+
+        file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
+        file.flush()
+        time.sleep(5)<br>file.close()
