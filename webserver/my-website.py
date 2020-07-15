c27efd648b8d86f5c68110cfd4d994fe5bfe5227
Stefan <stefanvanderheijden@gmail.com>
2020-07-15 20:58:35 +0200

    ds

--- webserver/my-website.py
+++ webserver/my-website.py
@@ -67,29 +67,30 @@ def action(pin, action):
 
    return render_template('main.html', **templateData)
 
-while True:
-   h1, t1 = dht.read_retry(dht.DHT22, 4)
-   h2, t2 = dht.read_retry(dht.DHT22, 5)
-   h3, t3 = dht.read_retry(dht.DHT22, 26)
 
-   h1 = round(h1, 2)
-   t1 = round(t1, 2)
-
-   h2 = round(h2, 2)
-   t2 = round(t2, 2)
-
-   h3 = round(h3, 2)
-   t3 = round(t3 ,2)
-
-   now = datetime.now()
-   file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
-   print("I have logged the temperature to a CSV file")
-   file.flush()
-   
-   time.sleep(5)
-   file.close()
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
    print("app host started")
 
+   while True:
+      h1, t1 = dht.read_retry(dht.DHT22, 4)
+      h2, t2 = dht.read_retry(dht.DHT22, 5)
+      h3, t3 = dht.read_retry(dht.DHT22, 26)
+   
+      h1 = round(h1, 2)
+      t1 = round(t1, 2)
+   
+      h2 = round(h2, 2)
+      t2 = round(t2, 2)
+   
+      h3 = round(h3, 2)
+      t3 = round(t3 ,2)
+   
+      now = datetime.now()
+      file.write(str(now)+","+str(t1)+","+str(h1)+","+str(t2)+","+str(h2)+","+str(t3)+","+str(h3)+"\n")
+      print("I have logged the temperature to a CSV file")
+      file.flush()
+      
+      time.sleep(5)
+      file.close()
